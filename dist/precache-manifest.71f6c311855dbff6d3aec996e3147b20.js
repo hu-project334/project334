66e7ca4fd5f88bad68cb52f4822bbdc502a62e2d
jayh475 <jayh.decuba@student.hu.nl>
2022-02-18 19:13:48 +0100

    adding dist subtree

---
+++ dist/precache-manifest.71f6c311855dbff6d3aec996e3147b20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c898fa30b6009b1b0a16",
+    "url": "/334-HOGESCHOOL-UTRECHT-INSTITUUT-VOOR-BEWEGINGSSTUDIES/css/app.8c5894f8.css"
+  },
+  {
+    "revision": "965345ec3f6772854d3e7dfe559eff67",
+    "url": "/334-HOGESCHOOL-UTRECHT-INSTITUUT-VOOR-BEWEGINGSSTUDIES/img/logo.965345ec.png"
+  },
+  {
+    "revision": "a70bfc58af17cbaef5308dccf5ba3cc5",
+    "url": "/334-HOGESCHOOL-UTRECHT-INSTITUUT-VOOR-BEWEGINGSSTUDIES/index.html"
+  },
+  {
+    "revision": "c898fa30b6009b1b0a16",
+    "url": "/334-HOGESCHOOL-UTRECHT-INSTITUUT-VOOR-BEWEGINGSSTUDIES/js/app.2267674a.js"
+  },
+  {
+    "revision": "4034e88a68d18441d9fc",
+    "url": "/334-HOGESCHOOL-UTRECHT-INSTITUUT-VOOR-BEWEGINGSSTUDIES/js/chunk-vendors.b5af654e.js"
+  },
+  {
+    "revision": "64fcce68bb9a842e5e9067dbfe7fc9e2",
+    "url": "/334-HOGESCHOOL-UTRECHT-INSTITUUT-VOOR-BEWEGINGSSTUDIES/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/334-HOGESCHOOL-UTRECHT-INSTITUUT-VOOR-BEWEGINGSSTUDIES/robots.txt"
+  }
+]);
