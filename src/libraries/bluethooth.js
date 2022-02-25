39f27cc28aae383a60d4708b06b7f8da1de7f6dd
jayh475 <jayh.decuba@student.hu.nl>
2022-02-25 14:30:13 +0100

    alsjeblieft hier is de setup

---
+++ src/libraries/bluethooth.js
@@ -0,0 +1,5 @@
+function roundTo20() {
+  return 5;
+}
+
+export { roundTo20 };
---
+++ src/views/Feed.vue
@@ -0,0 +1,24 @@
+Register.vue - base vue
+<template>
+  <h1>alexander / milo</h1>
+
+  <p><button @click="getData()">get Data</button></p>
+</template>
+
+<script>
+import { roundTo20 } from "/src/libraries/bluethooth.js";
+export default {
+  name: "regster",
+  data() {
+    return {
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    getData() {
+      console.log(roundTo20(), "hoort 5 te returnen");
+    },
+  },
+};
+</script>
