6eaf18fd8a4d5bec0aef4d1fe93f4ccd9cdd281c
jayh475 <jayh.decuba@student.hu.nl>
2022-03-25 13:20:40 +0100

    before merge with main

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,9 @@
+// vue.config.js file to be place in the root of your repository
+
 module.exports = {
   // ...Other Vue config options here, possibly
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/334-HOGESCHOOL-UTRECHT-INSTITUUT-VOOR-BEWEGINGSSTUDIES/"
-      : "/",
-
   pwa: {
-    name: "sensortechnology voor de fysiotherapeut",
-    themeColor: "#e96851",
+    name: "project",
+    themeColor: "#d8222a",
   },
 };
