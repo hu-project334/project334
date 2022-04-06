5f896a21ad52046ad7dc1e0e6dba949ed28b93a2
Alex-D-G <60549983+Alex-D-G@users.noreply.github.com>
2022-04-06 19:43:34 +0200

    Sensor status display added

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,21 +1,22 @@
 Register.vue - base vue
 <template>
-  <h1>alexander / milo</h1>
+  <h1>XsensDotSensor Development</h1>
 
   <p><button @click="getData()">connect</button></p>
   <p><button @click="identify()">identify device</button></p>
   <h2>Battery level: <p id="batterylevel">0</p></h2>
-  <p><button @click="startRec()">start recording</button></p>
-  <p><button @click="stopRec()">stop recording</button></p>
+  <h2>Sensor status: <p id="sensorStatus">offline</p></h2>
   <p><button @click="startDataExport()">Export data</button></p>
+  <p><button @click="streamData()">Start real time streaming</button></p>
+  <p><button @click="stopDataStream()">stop real time streaming</button></p>
+  <h2>X: <p id="x-axis"> 0 </p></h2>
+  <h2>Y: <p id="y-axis"> 0 </p></h2>
+  <h2>Z: <p id="z-axis"> 0 </p></h2>
 </template>
 
 <script>
-import { findBluetoothDevices } from "/src/libraries/bluetooth.js";
 import { XsensDotSensor } from "/src/libraries/bluetooth.js";
-import { startRecording } from "/src/libraries/bluetooth.js";
-import { stopRecording } from "/src/libraries/bluetooth.js";
-import { exportData } from "/src/libraries/bluetooth.js";
+import { findBluetoothDevices, startRTStream, stopRTStream } from "/src/libraries/bluetooth.js";
 export default {
   name: "regster",
   data() {
@@ -24,6 +25,9 @@ export default {
       y: 0,
     };
   },
+  created() {
+    window.addEventListener('beforeunload', this.handler)
+  },
   methods: {
     getData() {
       findBluetoothDevices()
@@ -31,14 +35,17 @@ export default {
     identify() {
       XsensDotSensor.blinkDeviceLED()
     },
-    startRec() {
-      startRecording()
-    },
-    stopRec() {
-      stopRecording()
-    },
     startDataExport() {
-      exportData()
+      XsensDotSensor.downloadDataToCSV()
+    },
+    streamData() {
+      startRTStream()
+    },
+    stopDataStream() {
+      stopRTStream()
+    },
+    handler: function handler() {
+      stopRTStream()
     }
   },
 };
