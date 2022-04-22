69e326d4ac18b35e5f75add404933e010828c4f1
jayh475 <jayh.decuba@student.hu.nl>
2022-04-22 17:14:42 +0200

    graph function not finished yet

--- src/components/ReloadPWA.vue
+++ src/components/ReloadPWA.vue
@@ -1,12 +1,10 @@
 <template>
-  <div
-    v-if="offlineReady || needRefresh"
-    class="flex flex-wrap"
-    role="alert"
-  >
+  <div v-if="offlineReady || needRefresh" class="flex flex-wrap" role="alert">
     <div class="message mt-1">
       <span v-if="offlineReady"> App ready to work offline </span>
-      <span v-else>New content available, click on reload button to update.</span>
+      <span v-else
+        >New content available, click on reload button to update.</span
+      >
     </div>
     <div class="buttons flex align-middle mt-2 md:mt-0">
       <button
@@ -16,18 +14,14 @@
       >
         Reload
       </button>
-      <button
-        class="button"
-        @click="closeButtonAction()"
-      >
-        Close
-      </button>
+      <button class="button" @click="closeButtonAction()">Close</button>
     </div>
   </div>
 </template>
 
 <script>
 // https://rubenr.dev/en/pwa-vite/
+// with composition api
 import { useRegisterSW } from "virtual:pwa-register/vue";
 const { updateServiceWorker } = useRegisterSW();
 
---
+++ src/components/tiles/charts/MovementPercentageInTime.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- x,y -->
+  <line-chart
+    :data="dataProp"
+    xtitle="data"
+    ytitle="Bewegingspercentage"
+  ></line-chart>
+</template>
+
+<script>
+export default {
+  name: "MovementPercentageInTime",
+  props: {
+    dataProp: Array,
+  },
+  data() {
+    return {
+      //   data1: { "2021-01-01": 10, "2021-01-02": 6 },
+    };
+  },
+  mounted() {
+    console.log(this.dataProp);
+  },
+};
+</script>
+
+<style scoped></style>
