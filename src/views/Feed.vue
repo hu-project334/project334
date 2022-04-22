7ee396761e7f844f213930b8ac83cc1f99ac3055
Milo Beliën <2002milo@gmail.com>
2022-04-22 15:34:25 +0200

    Splitted xsens dot communication files into seperate files

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -29,7 +29,7 @@ Register.vue - base vue
 <script>
 import NavBarTop from '../components/navbars/NavBarTop.vue';
 import { XsensDotSensor } from "/src/libraries/bluetooth.js";
-import { findBluetoothDevices, startRTStream, stopRTStream, getSyncStatusSensor } from "/src/libraries/bluetooth.js";
+import { findBluetoothDevices, startRTStream, stopRTStream, getSyncStatusSensor } from "/src/libraries/interface.js";
 export default {
   name: "Feed",
   components: {
